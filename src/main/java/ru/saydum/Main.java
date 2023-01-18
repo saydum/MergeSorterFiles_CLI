97b9748a4956f9d6c0c1dba929ca313e48b34d05
saydum <saydum.it5@gmail.com>
2023-01-18 18:03:58 +0300

    Created parser cmd

--- src/main/java/ru/saydum/Main.java
+++ src/main/java/ru/saydum/Main.java
@@ -1,7 +1,30 @@
 package ru.saydum;
 
+import ru.saydum.cmd.ParserCmd;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static boolean isAscending = true;
+    public static boolean isString = true;
+    public static String encoding = "utf-8";
+    public static String outputFileName = "";
+    public static List<String> inputFileNames = new ArrayList<>();
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // CMD Parser
+        new ParserCmd(args);
+
+        // Reader in1.txt in2.txt...
+//        Reader readerFiles =  new Reader(String[] files);
+
+        // Sorter -i -s (-a --abs, -d --desc)
+//        Sorter sorterIntFiles = new Sorter(ArrayList<Integer> readerFiles, boolean stateSorter);
+
+        // Sorter sorterStringFiles = new Sorter(ArrayList<Integer> readerFiles, boolean stateSorter);
+
+        // Writer out.txt
+//        Writer writer = new Writer(sorterIntFiles);
     }
 }
---
+++ src/main/java/ru/saydum/cmd/ParserCmd.java
@@ -0,0 +1,119 @@
+/**
+ *
+ * Код скопирован отсюда
+ * https://github.com/SuvorovJA/FS6926/blob/master/src/main/java/ru/sua/fs6926/ParseCommandLine.java
+ * */
+package ru.saydum.cmd;
+
+import org.apache.commons.cli.*;
+import ru.saydum.Main;
+
+import java.util.List;
+
+public class ParserCmd {
+    public ParserCmd(String[] args) {
+        parser(args);
+    }
+
+    private void parser(String[] args) {
+        Options opt = new Options();
+
+        opt.addOption(
+                "s",
+                false,
+                "Файлы содержат строки. Обязателый параметр, взаимоисключительна с -i."
+        );
+
+        opt.addOption(
+                "i",
+                false,
+                "Файлы содержат целые числа. Обязателый параметр, взаимоисключительна с -s."
+        );
+
+        opt.addOption(
+                "a",
+                false,
+                "Сортировка по возрастанию. Используется по умолчанию при отсутствии -a или -d."
+        );
+
+        opt.addOption(
+                "d",
+                false,
+                "Сортировка по убыванию. Необязательный параметр как -a."
+        );
+
+        opt.addOption(
+                "w",
+                false,
+                "Файлы ожидаются в кодировке CP1251. Необязательный параметр по умолчпнии используется UTF-8."
+        );
+
+        opt.addOption(
+                "h",
+                "help",
+                false,
+                "Справочник."
+        );
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = null;
+
+        try {
+            cmd = parser.parse(opt, args);
+        } catch (UnrecognizedOptionException e) {
+            System.out.println("Неизвестная опция " + e.getOption());
+            usagePrintAndClose(opt, 1);
+        } catch (ParseException e) {
+            System.out.println("Сбой разбора командной строки " + e.getMessage());
+            usagePrintAndClose(opt, 2);
+        }
+
+        if (cmd == null) {
+            System.out.println("Сбой разбора командной строки по неведомой причине");
+            usagePrintAndClose(opt, 3);
+        } else {
+            if (cmd.hasOption('h') || args.length == 0) {
+                usagePrintAndClose(opt, 0);
+            }
+            if (!(cmd.hasOption('i') || cmd.hasOption('s'))) {
+                System.out.println("Отсутствует обязательная опция -s или -i");
+                usagePrintAndClose(opt, 4);
+            }
+            if (cmd.hasOption('i') && cmd.hasOption('s')) {
+                System.out.println("Должна быть только одна опция или -s или -i");
+                usagePrintAndClose(opt, 5);
+            }
+            if (cmd.hasOption('a') && cmd.hasOption('d')) {
+                System.out.println("Должна быть только одна опция или -a или -d");
+                usagePrintAndClose(opt, 6);
+            }
+        }
+
+        List<String> files = cmd.getArgList();
+        if (files.size() < 2) {
+            System.out.println("Отсутствует имя файла для результата, или хотя бы одно имя входного файла.");
+            usagePrintAndClose(opt, 7);
+        }
+
+        if (cmd.hasOption("d")) Main.isAscending = false;
+        if (cmd.hasOption("i")) Main.isString = false;
+        if (cmd.hasOption("w")) Main.encoding = "cp1251";
+        Main.outputFileName = files.get(0);
+        files.remove(0);
+        Main.inputFileNames = files;
+    }
+
+    private static void usagePrint(Options opt) {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("""
+                sort-it.exe [opt] output.file input.files... \n
+                output.file  Обязательное имя файла с результатом сортировки. \n
+                input.files  Один, или больше входных файлов.
+                """, opt);
+    }
+
+    private static void usagePrintAndClose(Options opt, int status) {
+        usagePrint(opt);
+        System.exit(status);
+    }
+}
